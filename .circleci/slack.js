fc488c00cbb67b1e84d81b99e965bcc77366d1da
Sébastien Lombard <sebastien@agorize.com>
2018-10-31 18:38:05 +0100

    ci(slack): add commit name

--- .circleci/slack.js
+++ .circleci/slack.js
@@ -8,10 +8,10 @@ axios.post(process.env.SLACK_WEBHOOK, {
       author_name: 'Agorize UIKit',
       title: `${process.env.CIRCLE_JOB} #${process.env.CIRCLE_BUILD_NUM}`,
       title_link: process.env.CIRCLE_BUILD_URL,
-      text: `${process.env.CIRCLE_BRANCH}`,
+      // text: `${process.env.CIRCLE_BRANCH}`,
+      text: `${process.argv[2]}`,
       footer: 'Cirle CI',
-      footer_icon: 'https://assets.brandfolder.com/otz5mn-bw4j2w-6jzqo8/element.png',
-      ts: new Date().getTime()
+      footer_icon: 'https://assets.brandfolder.com/otz5mn-bw4j2w-6jzqo8/element.png'
 		}
 	]
 })
