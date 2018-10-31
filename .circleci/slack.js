d506e9fd71756a69361c8485caa372142b12ef53
Sébastien Lombard <sebastien@agorize.com>
2018-10-31 18:22:47 +0100

    ci(slack): Add slack notifer

---
+++ .circleci/slack.js
@@ -0,0 +1,17 @@
+const axios = require('axios')
+
+axios.post(process.env.SLACK_WEBHOOK, {
+  username: '🤖 UIKit CI Bot',
+  attachments: [
+    {
+      color: '#2eb886',
+      author_name: 'Agorize UIKit',
+      title: `${process.env.CIRCLE_JOB} #${process.env.CIRCLE_BUILD_NUM}`,
+      title_link: process.env.CIRCLE_BUILD_URL,
+      text: `${process.env.CIRCLE_BRANCH}`,
+      footer: 'Cirle CI',
+      footer_icon: 'https://assets.brandfolder.com/otz5mn-bw4j2w-6jzqo8/element.png',
+      ts: new Date().getTime()
+		}
+	]
+})
