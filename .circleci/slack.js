0f90a256176286ddc4601f33822f29ffbfc4bd46
Sébastien Lombard <sebastien@agorize.com>
2018-11-02 10:44:54 +0100

    ci(slack): Add user icon

--- .circleci/slack.js
+++ .circleci/slack.js
@@ -2,13 +2,12 @@ const axios = require('axios')
 
 axios.post(process.env.SLACK_WEBHOOK, {
   username: '🤖 UIKit CI Bot',
+  icon_url: 'https://cdn.icon-icons.com/icons2/1371/PNG/512/robot02_90810.png',
   attachments: [
     {
       color: '#2eb886',
-      author_name: 'Agorize UIKit',
       title: `${process.env.CIRCLE_JOB} #${process.env.CIRCLE_BUILD_NUM}`,
       title_link: process.env.CIRCLE_BUILD_URL,
-      // text: `${process.env.CIRCLE_BRANCH}`,
       text: `${process.argv[2]}`,
       footer: 'Cirle CI',
       footer_icon: 'https://assets.brandfolder.com/otz5mn-bw4j2w-6jzqo8/element.png'
