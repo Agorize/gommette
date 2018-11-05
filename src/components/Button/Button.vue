e1e0d1342af66d2b627b7e848e09cf5c95722e53
Sébastien Lombard <sebastien@agorize.com>
2018-11-05 11:50:19 +0100

    docs(nav): Update navbar

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,92 @@
+<template functional>
+  <component
+    :is="props.tag"
+    class="btn"
+    :class="{
+      [props.variant]: Boolean(props.variant),
+      link: Boolean(props.href)
+    }"
+    :disabled="props.disabled"
+    :type="props.type"
+  >
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'AgoButton',
+  props: {
+    /**
+     * Tag of the dom object
+     */
+    tag: {
+      type: String,
+      default: 'button'
+    },
+    /**
+     * Color variant
+     */
+    variant: {
+      type: String,
+      default: null
+    },
+    /**
+     * Button size
+     */
+    size: {
+      type: String,
+      default: null
+    },
+    /**
+     * Vue router "router-link" options
+     */
+    to: {
+      type: [String, Object],
+      default: null
+    },
+    /**
+     * Link href
+     */
+    href: {
+      type: String,
+      default: null
+    },
+    /**
+     * Link target
+     */
+    target: {
+      type: String,
+      default: '_blank'
+    },
+    /**
+     * Disabled state
+     */
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * Button type
+     */
+    type: {
+      type: String,
+      default: 'button'
+    }
+  }
+}
+</script>
+
+<docs>
+```js
+<ago-button>
+  I’m a button
+</ago-button>
+```
+
+```js
+<ago-button variant="brand">
+  I’m a button
+</ago-button>
+```
+</docs>
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,10 +1,23 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
 
 module.exports = {
+  title: 'Agorize UI-Kit',
   components: './src/components/**/*.vue',
   exampleMode: 'expand',
   usageMode: 'expand',
   pagePerSection: true,
+  sections: [
+    {
+      name: 'Get started',
+      content: 'docs/get-started.md'
+    },
+    {
+      name: 'Components',
+      components: './src/components/*/*',
+      sectionDepth: 1
+    }
+  ],
   webpackConfig: {
     module: {
       rules: [
