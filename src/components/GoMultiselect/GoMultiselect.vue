eb02a4fe76a07b921bfafdfcbe1e80c49b14f5d6
Sébastien Lombard <sebastien@agorize.com>
2019-05-06 14:21:35 +0200

    PROD-972 feat(component): multiselect

---
+++ src/components/GoMultiselect/GoMultiselect.vue
@@ -0,0 +1,31 @@
+<script>
+import Multiselect from 'vue-multiselect'
+// export default Multiselect
+export default {
+  name: 'GoMultiselect',
+  extends: Multiselect
+}
+</script>
+
+<docs>
+```js
+const value = []
+const options = [
+  'Select option',
+  'options',
+  'selected',
+  'mulitple',
+  'label',
+  'searchable',
+  'clearOnSelect',
+  'touched'
+]
+
+<div>
+  <go-multiselect
+    v-model="value"
+    :options="options"
+  />
+</div>
+``
+</docs>
